b4b655e920c420a748f6659f8ce4486642a69f62
Dawid Mądrzyk <maddawid@gmail.com>
2023-12-06 02:07:41 +0100

    design the progress bar

--- progress_tracker/src/App.js
+++ progress_tracker/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [units, setunits] = useState(100)
+
   return (
     <div className="App">
-        <p>
-          Hello
-        </p>
+      <div className="container">
+        <div className="progressBar">
+          {Array.from({length: units}).map((_, index) => (
+            <div className="unit" key={index}>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
