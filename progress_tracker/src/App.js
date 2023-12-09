c49092efe10baf26297a7c6f82670f3dd135fb08
Mądrzyk <dawid.madrzyk@capgemini.com>
2023-12-09 20:09:43 +0100

    cloning from sec pc

--- progress_tracker/src/App.js
+++ progress_tracker/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import { useState } from "react"
 
 function App() {
-  const [units, setunits] = useState(100)
+  const [units, setunits] = useState([60, 100])
 
   return (
     <div className="App">
       <div className="container">
         <div className="progressBar">
-          {Array.from({length: units}).map((_, index) => (
+          {Array.from({length: units[0]}).map((_, index) => (
+              <div className="completed" key={index}>
+              </div>
+          ))}
+          {Array.from({length: units[1]-units[0]}).map((_, index) => (
             <div className="unit" key={index}>
             </div>
           ))}
