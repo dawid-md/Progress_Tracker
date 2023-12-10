06b9f85c7d33aaf75fec385db7bbfa3aadf33eb5
Mądrzyk <maddawid@gmail.com>
2023-12-10 01:15:35 +0100

    editing projects data

--- progress_tracker/src/App.js
+++ progress_tracker/src/App.js
@@ -5,9 +5,9 @@ import { getDatabase, ref, get, push, remove, update } from 'firebase/database'
 import { app } from './Config/firebase';
 
 function App() {
-  const [units, setunits] = useState([60, 100])
   const [projects, setprojects] = useState([])
   const [newProject, setNewProject] = useState({ name: '', progress: '', units: '' });
+  const [editingProjectId, setEditingProjectId] = useState(null);
   const [showAddProject, setShowAddProject] = useState(false);
 
   useEffect(() => {
@@ -35,17 +35,37 @@ function App() {
     }
   }
 
+  function editProject(project) {
+    setNewProject({ name: project.name, progress: project.completed, units: project.units });
+    setEditingProjectId(project.id);
+    setShowAddProject(true); 
+  }
+
   async function saveProject() {
     const db = getDatabase(app);
-    const projectsRef = ref(db); 
-    try {
-      await push(projectsRef, newProject);
-      console.log("Project saved");
-      setNewProject({ name: '', progress: '', units: '' }); //Reset input fields after saving
-      getProjects(); //Refresh list of projects
-    } catch (error) {
-      console.log(error);
+    let projectsRef;
+
+    if (editingProjectId) {
+      projectsRef = ref(db, editingProjectId);
+      try {
+        await update(projectsRef, newProject);
+        console.log("Project updated");
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      projectsRef = ref(db);
+      try {
+        await push(projectsRef, newProject);
+        console.log("Project saved");
+      } catch (error) {
+        console.log(error);
+      }
     }
+    setNewProject({ name: '', progress: '', units: '' }); // Reset input fields after saving
+    setEditingProjectId(null); // Reset editing ID
+    setShowAddProject(false); // Hide input fields
+    getProjects(); // Refresh list of projects
   }
 
   async function deleteProject(projectId) {
@@ -63,7 +83,17 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        {projects.map(item => <Project key={item.id} id={item.id} name={item.name} completed={item.progress} units={item.units} deleteProject={deleteProject} />)}
+        {projects.map(item => (
+          <Project
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            completed={item.progress}
+            units={item.units}
+            deleteProject={deleteProject}
+            editProject={editProject}
+          />
+        ))}
         <button onClick={() => setShowAddProject(!showAddProject)}>Add New Project</button>
         {showAddProject && (
           <div>
@@ -74,13 +104,13 @@ function App() {
               onChange={e => setNewProject({ ...newProject, name: e.target.value })}
             />
             <input 
-              type="text" 
+              type="number" 
               placeholder="Progress" 
               value={newProject.progress} 
               onChange={e => setNewProject({ ...newProject, progress: e.target.value })}
             />
             <input 
-              type="text" 
+              type="number" 
               placeholder="Units" 
               value={newProject.units} 
               onChange={e => setNewProject({ ...newProject, units: e.target.value })}
--- progress_tracker/src/Project.js
+++ progress_tracker/src/Project.js
@@ -1,4 +1,4 @@
-export default function Project({id, name, completed, units, deleteProject}){
+export default function Project({id, name, completed, units, deleteProject, editProject}){
     return(
         <div className='project_data'>
         <h2 className="project_name">{name}</h2>
@@ -8,12 +8,12 @@ export default function Project({id, name, completed, units, deleteProject}){
                 <div className="completed" key={index}>
                 </div>
             ))}
-            {Array.from({length: units}).map((_, index) => (
+            {Array.from({length: units-completed}).map((_, index) => (
               <div className="unit" key={index}>
               </div>
             ))}
           </div>
-          <button className="edit">edit</button>
+          <button className="edit" onClick={() => editProject({id, name, completed, units})}>edit</button>
           <button className="delete" onClick={() => deleteProject(id)}>delete</button>
         </div>
       </div>
